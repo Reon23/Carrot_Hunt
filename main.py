98e4fc6afff2a6486318228c439c6eb6a85cc3f8
Reon23 <166202651+Reon23@users.noreply.github.com>
2025-02-10 22:36:20 +0530

    Player bullets

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 
-from player import Player
+from player import Player, PlayerBullet
 
 # Constants
 SCREEN_WIDTH = 1280
@@ -8,6 +8,7 @@ SCREEN_HEIGHT = 720
 
 # Variables
 display_scroll = [0, 0]
+player_bullets = []
 
 # pygame setup
 pygame.init()
@@ -20,31 +21,50 @@ player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, 32, 32)
 
 while running:
     # poll for events
+    mouse_x, mouse_y = pygame.mouse.get_pos()
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+        # poll for mouse events
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                player_bullets.append(PlayerBullet(player.x, player.y, mouse_x, mouse_y))
     # poll for keyboard events
     keys = pygame.key.get_pressed()
 
+    
+
     if keys[pygame.K_a]:
         display_scroll[0] -= 5
+        for bullet in player_bullets:
+            bullet.x += 5
     if keys[pygame.K_d]:
         display_scroll[0] += 5
+        for bullet in player_bullets:
+            bullet.x -= 5
     if keys[pygame.K_w]:
         display_scroll[1] -= 5
+        for bullet in player_bullets:
+            bullet.y += 5
     if keys[pygame.K_s]:
         display_scroll[1] += 5
+        for bullet in player_bullets:
+            bullet.y -= 5
 
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill((184, 66, 78))
+    screen.fill("green")
 
     # RENDER YOUR GAME HERE
-    pygame.draw.rect(screen, "green", (300-display_scroll[0], 400-display_scroll[1], 16, 16))
-    pygame.draw.rect(screen, "green", (600-display_scroll[0], 600-display_scroll[1], 16, 16))
+    pygame.draw.rect(screen, "red", (300-display_scroll[0], 400-display_scroll[1], 16, 16))
+    pygame.draw.rect(screen, "red", (600-display_scroll[0], 600-display_scroll[1], 16, 16))
     player.render(screen);
+
+    for bullet in player_bullets:
+        bullet.render(screen)
+
     # flip() the display to put your work on screen
     pygame.display.flip()
 
--- player.py
+++ player.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 
 class Player:
     
@@ -11,3 +12,22 @@ class Player:
     
     def render(self, screen):
         pygame.draw.rect(screen, "blue", (self.x, self.y, self.width, self.height))
+
+class PlayerBullet:
+
+    #Initalize bullet variables
+    def __init__(self, x, y, mouse_x, mouse_y):
+        self.x = x
+        self.y = y
+        self.mouse_x = mouse_x
+        self.mouse_y = mouse_y
+        self.speed = 15
+        self.angle = math.atan2(y-mouse_y, x-mouse_x)
+        self.x_vel = math.cos(self.angle) * self.speed
+        self.y_vel = math.sin(self.angle) * self.speed
+
+    def render(self, screen):
+        self.x -= int(self.x_vel)
+        self.y -= int(self.y_vel)
+
+        pygame.draw.circle(screen, "black", (self.x, self.y), 5) 
