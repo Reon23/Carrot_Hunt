433060c725e219c39e96e86f36fa5bdd40592f64
Reon23 <166202651+Reon23@users.noreply.github.com>
2025-05-04 14:48:29 +0530

    cleanup

--- display.py
+++
@@ -1,6 +0,0 @@
-import pygame
-
-pygame.init()
-info = pygame.display.Info()
-DISPLAY_WIDTH, DISPLAY_HEIGHT = info.current_w, info.current_h
-SCREEN = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT), pygame.FULLSCREEN)
